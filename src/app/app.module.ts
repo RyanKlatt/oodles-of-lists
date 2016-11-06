fb919cfd83f3d6162d4d1ac5ad356895de07f213
Ryan Klatt <rpklatt@msn.com>
2016-11-06 15:02:22 -0700

    Added new pages and data providers to app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,33 @@
 import { NgModule } from '@angular/core';
 import { IonicApp, IonicModule } from 'ionic-angular';
 import { MyApp } from './app.component';
+import { Storage } from '@ionic/storage';
 import { HomePage } from '../pages/home/home';
+import { IntroPage } from '../pages/intro-page/intro-page';
+import { ChecklistPage } from '../pages/checklist-page/checklist-page';
+import { Data } from '../providers/data';
 
 @NgModule({
-  declarations: [
-    MyApp,
-    HomePage
-  ],
-  imports: [
-    IonicModule.forRoot(MyApp)
-  ],
-  bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    HomePage
-  ],
-  providers: []
+declarations: [
+MyApp,
+HomePage,
+IntroPage,
+ChecklistPage
+],
+
+imports: [
+IonicModule.forRoot(MyApp)
+],
+
+bootstrap: [IonicApp],
+entryComponents: [
+MyApp,
+HomePage,
+IntroPage,
+ChecklistPage
+],
+
+providers: [Storage, Data]
 })
+
 export class AppModule {}
