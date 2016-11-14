d48383d990e031ea9fb3481edc02f2d18ff6c7cd
Ryan Klatt <rpklatt@msn.com>
2016-11-14 14:23:29 -0700

    Working on settings page

--- src/pages/settings/settings.ts
+++ src/pages/settings/settings.ts
@@ -1,22 +1,24 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
-/*
-  Generated class for the Settings page.
-
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
-*/
 @Component({
   selector: 'page-settings',
   templateUrl: 'settings.html'
 })
 export class SettingsPage {
 
-  constructor(public navCtrl: NavController) {}
+  private color: string;
+
+  constructor(public navCtrl: NavController) {
+  	this.color = 'dark';
+  }
 
   ionViewDidLoad() {
-    console.log('Hello SettingsPage Page');
+    
+  }
+
+  changeColor(color:string) {
+    this.color = color;
   }
 
 }
